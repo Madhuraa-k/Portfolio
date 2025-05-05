97959e5ea2779c439a8c167bd8ea0a93df5366d9
Madhura Kanfade <madhura@Madhuras-MacBook-Air.local>
2025-05-05 14:30:24 +0530

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:madhura.kanfade22@spit.ac.in">madhura.kanfade22@spit.ac.in</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/madhura-kanfade-28816a249/">linkedin.com/Madhura-Kanfade</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Madhuraa-k">github.com/Madhuraa-k</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
